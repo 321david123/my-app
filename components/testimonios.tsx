de222ac61dbc580fe8d0de341696048a06e2dd64
David Martinez Rodríguez <321davidmr123@gmail.com>
2025-03-23 00:48:02 -0500

    more

--- components/testimonios.tsx
+++ components/testimonios.tsx
@@ -11,42 +11,52 @@ export default function Testimonios() {
   const testimonies = [
     {
       name: "Anónimo",
-      year: "17",
+      year: "XVII",
       quote: "Ser parte de célula me hizo crecer profesionalmente así como también crecer como persona y ser humano.",
     },
     {
       name: "Juan Martin Arriaga Alejos",
-      year: "16",
+      year: "XVI",
       quote: "Fue un lugar en el cual descubrí parte de mi pasión en esta vida. Dónde podía aprender siendo un niño todavía. Grandes experiencias viví y recuerdos que se quedarán.",
     },
     {
+      name: "Valeria Rivera",
+      year: "XII",
+      quote: "Estar en Célula ha sido mi mejor experiencia en la vida académica, no solo me enamoré de la tecnología, programación y redes, sino que también hice las mejores amistades y experiencias. Mejoré como estudiante y como persona.",
+    },
+    {
       name: "Pedro González Arriaga",
-      year: "13",
+      year: "XIII",
       quote: "Excelente. En todo momento, el profesor Justo tuvo un enfoque muy humano y pedagógico, algo que hace mucha falta en las escuelas, especialmente en el ámbito de la tecnología. Aunque ya no me dedico a la programación y ahora estoy enfocado en las artes, las habilidades y experiencias que adquirí me acompañarán toda la vida. Aprendí que soy capaz de llevar a cabo proyectos desafiantes y concretarlos con éxito. Además, viví experiencias extraordinarias, como viajes y escaladas, que no habrían sido posibles sin el compromiso y la pasión del profesor Justo.",
     },
     {
       name: "Gerardo Antonio Correa Álvarez",
-      year: "15",
+      year: "XV",
       quote: "Mi experiencia en célula fue algo demasiado especial, si bien yo ya sabía lo que me interesaba para estudiar en la Universidad y que definitivamente era relacionado a las computadoras, a la hora de entrar al Cecyte Cortázar no me esperaba que la experiencia fuera tan grata, Justo demostró ser un profesor que hizo que mi interés se captara completamente en los proyectos y actividades que nos planteó, los conocimientos que me hizo aprender son cosas que hasta hoy en día sigo aplicando y agradezco infinitamente, aún más allá que los conocimientos, las experiencias que tuve dentro son invaluables, gracias por todo a mis profesores y compañeros de generación.",
     },
     {
       name: "Lau",
-      year: "17",
+      year: "XVII",
       quote: "Al ser parte del programa célula lo que más valoro es el aprender sobre cómo formar parte de una comunidad o grupo estudiantil, siendo de gran ayuda para aprender a comunicarme y trabajar colaborativamente, ya sea por un objetivo en común dentro del campo de las STEM o por brindar ayuda en orientar a alguien a conseguir una meta, e incluso ser solidaria y tener un sentido de unión, aspectos fundamentales para reunir memorias y experiencias valiosas y muchas lecciones aprendidas que nos ayudan a subir peldaños en nuestras carreras o vidas, lo que se agradece como herramienta en diversos entornos, como el institucional o laboral, de manera que me hizo crecer no solo profesional sino también como humana.",
     },
     {
       name: "Jesús Jiménez",
-      year: "17",
+      year: "XVII",
       quote: "Gracias a Celula, me formé como un mejor estudiante, cambié mi perspectiva de lo que es ser estudiante, gracias a eso estoy en la universidad que siempre quise estar, le doy las gracias a todo el equipo de Celula, pero en especial al maestro Justo 72.",
     },
     {
+      name: "Arturo Macias",
+      year: "X",
+      quote: "Un programa que fomenta la verdadera educación, desde la vocación y el compromiso, una gran plataforma de conocimiento.",
+    },
+    {
       name: "Carlos Eduardo",
-      year: "17",
+      year: "XVII",
       quote: "El haber estado en célula me hace sentir muy orgulloso, en célula logramos ampliar nuestro panorama y hacer cosas que no veíamos que pudiéramos lograr en la escuela, célula me dejó muchos conocimientos, pero también me dejó experiencias increíbles y grandes amistades con las cuales he crecido, estoy sumamente agradecido con los maestros por habernos dado la oportunidad de formar la última generación célula, los llevaré siempre en mi corazón.",
     },
     {
       name: "Grecia Márquez",
-      year: "17",
+      year: "XVII",
       quote: "Cuando puse un pie en célula, me sentí como parte de, aparte de aprender sobre muchas cosas nuevas y conocer a gente muy linda",
     },
     {
@@ -64,6 +74,13 @@ export default function Testimonios() {
       year: "XII",
       quote: "Recuerdo cuando en sesiones de WER se sacaban unos chistes muy buenos y el compañerismo se notaba demasiado.",
     },
+    {
+      name: "Grecia Sanjuanico",
+      year: "XVII",
+      quote: "Célula para mi fue y sigue siendo una familia en la que aprendí muchísimo y me hizo amar lo que hacía, así como la pasión por las cosas que nos gustan.",
+    },
+
+
   ]
 
   return (
