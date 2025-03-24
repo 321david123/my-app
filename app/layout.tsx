f083cd9ad2f8914608ea50eb2729f0956cb99741
David Martinez Rodríguez <321davidmr123@gmail.com>
2025-03-24 01:06:08 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="https://miguelin777.github.io/Clubdel100/assets/image/fix2.png" />
       <body>
       <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-E9P71GREY7"
