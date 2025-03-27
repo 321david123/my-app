35600592ea6ec3fedce219ef269e798a846d255c
David Martinez Rodríguez <321davidmr123@gmail.com>
2025-03-27 17:43:58 -0500

    more

--- components/closure.tsx
+++ components/closure.tsx
@@ -2,7 +2,7 @@ export default function Closure() {
   return (
     <div className="bg-gray-100 py-16">
       <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-sans font-bold mb-8">El Legado Continúa</h2>
+        <h2 className="text-4xl font-bold mb-8">El Legado Continúa</h2>
         <p className="text-xl text-gray-800 max-w-3xl mx-auto mb-8">
           Aunque el viaje de Célula ha llegado a su fin, su impacto resuena a través de las vidas que tocó y las
           innovaciones que inspiró. El espíritu de curiosidad, colaboración y excelencia que definió a Célula continúa
